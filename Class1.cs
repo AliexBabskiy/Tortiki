e697e8eb741114602508447882ebb24e00707634
AliexBabskiy <ababskiy@gmail.com>
2023-11-08 18:38:01 +0300

    первый вариант

---
+++ Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cl1
+{
+    internal class Menu
+    {
+        private int minStrelochka;
+        private int maxStrelochka;
+
+        public Menu(int min, int max)
+        {
+            minStrelochka = min;
+            maxStrelochka = max;
+        }
+        public static int men(int minStrelochka, int maxStrelochka)
+        {
+            int post = 1;
+            ConsoleKeyInfo info;
+            Console.CursorVisible = false;
+            do
+            {
+                Console.SetCursorPosition(0, post);
+                Console.WriteLine("->");
+
+                info = Console.ReadKey();
+
+                Console.SetCursorPosition(0, post);
+                Console.WriteLine("  ");
+
+                if (info.Key == ConsoleKey.UpArrow && post != minStrelochka)
+                {
+                    post--;
+                }
+                else if (info.Key == ConsoleKey.DownArrow && post != maxStrelochka)
+                {
+                    post++;
+                }
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return post;
+        }
+    }
+}
+
+namespace Cl2
+{
+    internal class Tortik
+    {
+        public string Forma;
+        public int Razmer;
+        public string Taste;
+        public string Glazur;
+        public string Decor;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿// добавить дату, и добавить её в файл (нужно узнать нужно ли это) (Data now) (можно взять из ежедневника)    ^<>^
+// добавить начало и конец на ентер и ескейп     ^<>^
+//    добавить проверку наличия файла история заказов, если он есть то дополнить новый заказ, если нет, то создать заново  (возможно не делать добавление срезу ту а сделать его позже, в стрелочном мненю выбора (сначала просто создать файл, а потом с трелочном меню начать его дополнять))  (СДЕЛАНО ТОЛЬКО ОТЧАСТИ ТАК КАК ЗАПИСЬ В ФАЙЛ УЖЕ СОЗДАЁТСЯ ИЛИ ИСПОЛЬЗУЕТСЯ)   ^<>^ 
+
+//  ДОБАВИТЬ ЗАПИСЬ В КОНЕЦ ФАЙЛА, А ТО ОН БУДЕТ ПЕРЕЗАПИСЫВАТЬСЯ
+
+//    добавить стрелочное меню из календыря, желательно адаптировать его в классы    ^<>^
+//    добавить критерии тортов из одной из работ в классе (пицаа)
+//    сохранить в массив развые позиции заказа, а потом сложить их
+//    добавить при завершении заказа добавление в файл сумму заказа (возможно его состав(тогда придётся добавить в массив к стоимости позиций и их название, либо создать новый))   (массивы наверное сделать сверху)
+
+//    добавление создания нового заказа   ^<>^
+//  если да то сделать перенос в стрелочное меню и обнулить массив со стоимостью (составом прошлого заказа)   ^<>^
+//  если нет то завершить заказ  ^<>^
+// int post = Menu.men(1, 5);
+
+using System;
+using Cl1;
+using Cl2;
+
+class Calculator
+{
+    static DateTime now = DateTime.Now;            // ${DateTime.Now}  (Для вывода)
+    static string userName = Environment.UserName;      // Определение имени усройства
+    static string txt;                                  // переменная для записи в файл данных
+
+    static void Main()
+    {
+        Console.WriteLine("Добро пожаловать. Нажмите на Enter клавишу для запуска.\n");
+        while (true)
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+        }
+        Console.Clear();
+
+        Tortik 
+
+        txt = $"{DateTime.Now}";                                                                                       //  МЕСТО ВВОДА ДАННЫХ В ФАЙЛ
+        
+        Console.WriteLine(txt);            // удалить позже или заменить
+
+        string path = $"C:\\Users\\{userName}\\Desktop\\История заказов.txt";
+        if (File.Exists(path))
+        {
+            ZapisON();
+        }
+        else
+        {
+            ZapisOFF();
+        }
+
+        Noviy();
+    }
+
+    static void ZapisOFF()                                                                                             // БЛОК ЗАПИСИ В ФАЙЛ
+    {
+        File.WriteAllText($"C:\\Users\\{userName}\\Desktop\\История заказов.txt", txt);
+    }
+
+    static void ZapisON()                                                                                             // БЛОК ЗАПИСИ В КОНЕЦ ФАЙЛА
+    {
+        File.AppendAllText($"C:\\Users\\{userName}\\Desktop\\История заказов.txt", $"\n{txt}");
+    }
+
+    static void Exit()                                                                        // БЛОК ВЫХОДА
+    {
+        Console.WriteLine("Завершение работы нажмите Escape.");
+        while (true)
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                return;
+            }
+            else
+            {
+                Console.Clear();
+                Main();
+            }
+        }
+    }
+
+    static void Noviy()                                                                        // БЛОК ВЫБОРА НОВОГО ЗАКАЗА (ВОЗМОЖНО ПРИДЁТСЯ ПЕРЕНЕСТИ В MAIN)
+    {
+        Console.WriteLine("Если хотите создать новый заказ нажмите Enter, если нет то нажмите Escape.");
+        while (true)
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                return;
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                Main();
+            }
+        }
+    }
+}
